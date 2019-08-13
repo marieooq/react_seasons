01ab08e98443350cae25f7bb019090bfa80c5c2a
Marie Otaki <otakimarie@gmail.com>
2019-08-12 20:16:13 -0700

    modified files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonsDisplay from './SeasonsDisplay';
 
-const App = () =>{
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
-  return <div>Hi, there!</div>
-};
+class App extends React.Component{
+  // constructor(props){
+  //   super(props);
+
+  //   this.state = {lat: null, errorMessage: ''};
+  // }
+
+  state = {lat: null, errorMessage: ''};
+
+  componentDidMount(){
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({lat: position.coords.latitude}),
+      err => this.setState({errorMessage: err.message})
+    );
+  }
+
+  componentDidUpdate(){
+    console.log('My component was just updated - it rendered!');
+  }
+
+  //React says we have to define render!!
+  render(){
+    if(this.state.errorMessage && !this.state.lat){
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    
+    if(!this.state.errorMessage && this.state.lat){
+      return <SeasonsDisplay lat={this.state.lat} />;
+    }
+    
+    return <div>Loading!</div>;
+    
+  }
+}
 
 ReactDOM.render(
   <App />,
