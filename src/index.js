8f061c80bce31847ef72b2617d618f78d953b1f3
Marie Otaki <otakimarie@gmail.com>
2019-08-13 18:12:07 -0700

    modified index.js

--- src/index.js
+++ src/index.js
@@ -23,8 +23,7 @@ class App extends React.Component{
     console.log('My component was just updated - it rendered!');
   }
 
-  //React says we have to define render!!
-  render(){
+  renderContent(){
     if(this.state.errorMessage && !this.state.lat){
       return <div>Error: {this.state.errorMessage}</div>;
     }
@@ -35,6 +34,15 @@ class App extends React.Component{
     
     return <Spinner message="Please accept location request"/>;
     
+  }
+
+  //React says we have to define render!!
+  render(){
+    return (
+      <div className="border red">
+        {this.renderContent()}
+      </div>
+    );
   }
 }
 
